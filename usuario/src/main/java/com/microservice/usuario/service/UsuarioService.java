cc03e82f11e833fa8a14bbb0c1f9f9f91d55e6ad
Ykko115 <nicoramosdiaz2111@gmail.com>
2026-04-20 15:09:44 -0400

    implementacion completada del microservicio Usuario

---
+++ usuario/src/main/java/com/microservice/usuario/service/UsuarioService.java
@@ -0,0 +1,15 @@
+package com.microservice.usuario.service;
+import java.util.List;
+import com.microservice.usuario.entitie.Usuario;
+
+public interface UsuarioService {
+
+    Usuario crear(Usuario usuario);
+    boolean existsByEmail(String email);
+    boolean existsByRut(String rut);
+    Usuario ObtenerPorId(Long id);
+    List<Usuario> listarTodos();
+    void eliminar(Long id);
+    Usuario actualizar(Long id, Usuario usuarioActualizado);
+}
+
