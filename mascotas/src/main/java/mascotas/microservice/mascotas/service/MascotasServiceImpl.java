d688ada1d569c1c1b9e5a20e4e010b57b3107dd1
Ykko115 <nicoramosdiaz2111@gmail.com>
2026-06-08 11:30:48 -0400

    Implementacion de nuevos test del servicio mascotas

--- mascotas/src/main/java/mascotas/microservice/mascotas/service/MascotasServiceImpl.java
+++ mascotas/src/main/java/mascotas/microservice/mascotas/service/MascotasServiceImpl.java
@@ -15,7 +15,6 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 import mascotas.microservice.mascotas.client.MascotaMatcherClient;
 import mascotas.microservice.mascotas.dto.MatchResultDTO;
 import mascotas.microservice.mascotas.entity.Mascotas;
-import mascotas.microservice.mascotas.entity.Mascotas.Especie;
 import mascotas.microservice.mascotas.repository.MascotasRepository;
 
 @Service
